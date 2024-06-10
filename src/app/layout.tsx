3eab4e940b91067108050d46d1a058584876be41
Donovan Dikaio <d@dika.io>
2024-06-09 23:28:02 -0700

    add next-theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import { GeistMono as geist_mono } from "geist/font/mono";
 import { GeistSans as geist_sans } from "geist/font/sans";
 import type { Metadata } from "next";
 import "@/styles/tailwind.css";
+import { Providers } from "@/components/providers";
+import { Toaster } from "@/components/ui/sonner";
 
 export const metadata: Metadata = {
 	title: "Create Next App",
@@ -15,11 +17,17 @@ export default function RootLayout({
 	children: React.ReactNode;
 }>) {
 	return (
-		<html lang="en" suppressHydrationWarning>
-			<body
-				className={cn("min-h-screen", geist_sans.variable, geist_mono.variable)}
-			>
-				{children}
+		<html lang="en" suppressHydrationWarning className="h-full">
+			<body className={cn("h-full", geist_sans.variable, geist_mono.variable)}>
+				<Providers
+					attribute="class"
+					defaultTheme="system"
+					enableSystem
+					disableTransitionOnChange
+				>
+					{children}
+				</Providers>
+				<Toaster />
 			</body>
 		</html>
 	);
